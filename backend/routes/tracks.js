b45470b243c610d5ac16a5c8cd1a21d3a769e701
ckb17170 <nicole.haggerty.2017@uni.strath.ac.uk>
2022-01-24 22:38:14 +0000

    fixed error with ID

--- backend/routes/tracks.js
+++ backend/routes/tracks.js
@@ -1,13 +1,14 @@
-// routes/players.js
+// routes/tracks.js
 
 const express = require("express");
+const { vary } = require("express/lib/response");
 const router = express.Router();
-let players = require("../dummyDatabase");
+let tracks = require("../dummyDatabase");
 
 router.get("/list", async (req, res) => {
   try {
     res.status(200).json({
-      data: players
+      data: tracks
     });
   } catch (err) {
     res.status(400).json({
@@ -21,9 +22,9 @@ router.get("/:artist", async (req, res) => {
   let { artist } = req.params;
   artist = String(artist);
   try {
-    let player = players.filter(player => player.artist.toLowerCase() === artist.toLowerCase());
+    let track = tracks.filter(track => track.artist.toLowerCase() === artist.toLowerCase());
     res.status(200).json({
-      data: player
+      data: track
     });
   } catch (err) {
     res.status(400).json({
@@ -37,9 +38,25 @@ router.get("/title/:title", async (req, res) => {
     let { title } = req.params;
     title = String(title);
     try {
-      let player = players.filter(player => player.title.toLowerCase() === title.toLowerCase());
+      let track = tracks.filter(track => track.title.toLowerCase() === title.toLowerCase());
       res.status(200).json({
-        data: player
+        data: track
+      });
+    } catch (err) {
+      res.status(400).json({
+        message: "Some error occured",
+        err
+      });
+    }
+  });
+
+  router.get("/id/:id", async (req, res) => {
+    let { id } = req.params;
+    console.log(id);
+    try {
+      let track = tracks.filter(track => track.id === Number(id));
+      res.status(200).json({
+        data: track
       });
     } catch (err) {
       res.status(400).json({
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,39 +4,56 @@ export default function App() {
   const [list, setList] = useState(true);
   const [list1, setList1] = useState(false);
   const [list2, setList2] = useState(false);
-  const [players, setPlayers] = useState([]);
-  const [players1, setPlayers1] = useState([]);
-  const [players2, setPlayers2] = useState([]);
+  const [list3, setList3] = useState(false);
+  const [tracks, settracks] = useState([]);
+  const [tracks1, settracks1] = useState([]);
+  const [tracks2, settracks2] = useState([]);
+  const [tracks3, settracks3] = useState([]);
   const [name, setName] = useState('')
+  const [ID, setID] = useState(0)
   const [theArtist, settheArtist] = useState('')
 
 
   useEffect(() => {
-    fetch("http://localhost:3001/players/list")
+    fetch("http://localhost:3001/tracks/list")
       .then((response) => response.json())
       .then((responseJson) => {
-        setPlayers(responseJson.data);
+        settracks(responseJson.data);
       });
   }, []);
   
 
   let getTitle = () => {
-    fetch(`http://localhost:3001/players/title/${name}`)
+    fetch(`http://localhost:3001/tracks/title/${name}`)
       .then((response) => response.json())
       .then((responseJson) => {
-        setPlayers2(responseJson.data);
+        settracks2(responseJson.data);
         setList(true);
         setList1(false)
         setList2(true);
+        setList3(false);
       });
   };
 
   let getArtist = () => {
-    fetch(`http://localhost:3001/players/${theArtist}`)
+    fetch(`http://localhost:3001/tracks/${theArtist}`)
       .then((response) => response.json())
       .then((responseJson) => {
-        setPlayers1(responseJson.data);
+        settracks1(responseJson.data);
         setList1(true);
+        setList(true);
+        setList2(false);
+        setList3(false);
+      });
+  };
+
+  let getID = () => {
+    fetch(`http://localhost:3001/tracks/id/${ID}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        settracks3(responseJson.data);
+        setList1(false);
+        setList3(true);
         setList(true);
         setList2(false);
       });
@@ -61,12 +78,33 @@ export default function App() {
       <button  onClick={() => getArtist()}>
         Search
       </button>
+
+      <h1>Search ID</h1>
+      <input 
+      type="number"
+      name="name"
+      onChange={event => setID(event.target.value)} />
+      <button  onClick={() => getID()}>
+        Search
+      </button>
+
       {list2 ? (
         <div className="list-group">
           <h1><b>Results:</b></h1>
-          {players2.map((player) => (
+          {tracks2.map((track) => (
             <li>
-              <b>Artist:</b> { player.artist }, <b>Title:</b> { player.title }
+              <b>Artist:</b> { track.artist }, <b>Title:</b> { track.title }, <b>ID:</b> { track.id }
+            </li>
+          ))}
+          <br></br>
+        </div>
+      ) : null}
+      {list3 ? (
+        <div className="list-group">
+          <h1><b>Results:</b></h1>
+          {tracks3.map((track) => (
+            <li>
+              <b>Artist:</b> { track.artist }, <b>Title:</b> { track.title }, <b>ID:</b> { track.id }
             </li>
           ))}
           <br></br>
@@ -75,10 +113,10 @@ export default function App() {
       {list1 ? (
         <div className="list-group">
           <h1><b>Results:</b></h1>
-          {players1.map((player) => (
+          {tracks1.map((track) => (
             <li>
               
-              <b>Artist:</b> { player.artist }, <b>Title:</b> { player.title }
+              <b>Artist:</b> { track.artist }, <b>Title:</b> { track.title }, <b>ID:</b> { track.id }
             </li>
           ))}
           <br></br>
@@ -89,9 +127,9 @@ export default function App() {
         <div className="list-group">
           <br></br>
           <h1><b>Track List:</b></h1>
-          {players.map((player) => (
+          {tracks.map((track) => (
             <li>
-              <b>Artist:</b> {player.artist }, <b>Title:</b> { player.title }
+              <b>Artist:</b> {track.artist }, <b>Title:</b> { track.title }, <b>ID:</b> { track.id }
             </li>
           ))}
 
