04e1a074b91239f02bc981504e33b17643602746
nicole-haggerty <45008803+nicole-haggerty@users.noreply.github.com>
2023-09-12 11:39:01 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const port = process.env.PORT || 3001;
-const playersRouter = require("./routes/players");
+const tracksRouter = require("./routes/tracks");
 
 app.use(logger("dev"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/players", playersRouter);
+app.use("/tracks", tracksRouter);
 app.listen(port, function() {
   console.log("Runnning on " + port);
 });
